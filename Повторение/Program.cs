3ab567b4c72c0b51cae653d391d4407527ece5ec
KovDan <koval_dan2004@mail.ru>
2025-04-08 20:34:58 +0700

    Площадь найдена

--- Повторение/Program.cs
+++ Повторение/Program.cs
@@ -1,9 +1,21 @@
 ﻿
+#region begin task-1
 Console.Write("Введите сторону квадрата: ");
 string a = Console.ReadLine();
-if (int.TryParse(a, out int number))
+bool result = int.TryParse(a, out int number);
+if (result)
 {
     int p = number * 4;
     Console.WriteLine($"Периметр квадрата со стороной a={a} равана {p}");
 }
+#endregion
+
+#region begin task-2
+if (result)
+{
+    double dNumber = Convert.ToDouble(number);
+    double s = Math.Pow(dNumber, 2);
+    Console.WriteLine($"Площадь квадрата со стороной a={a} равана {s}");
+}
+#endregion
 
