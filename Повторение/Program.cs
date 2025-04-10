dddb386236359ae4b7ad9dc48f87fd455119e2b4
KovDan <koval_dan2004@mail.ru>
2025-04-10 18:29:06 +0700

    Были напимны тесты для 1-2 и 4 заданий

--- Повторение/Program.cs
+++ Повторение/Program.cs
@@ -1,221 +1,263 @@
-﻿
+﻿public class Program
 {
-    #region begin task-1
-    Console.WriteLine($"===== Задание 1 =====");
-    Console.Write("Введите сторону квадрата: ");
-    string a = Console.ReadLine();
-    bool result = int.TryParse(a, out int number);
-    if (result)
+    public static void Main(string[] str)
     {
-        int p = number * 4;
-        Console.WriteLine($"Периметр квадрата со стороной a={a} равана {p}");
-    }
-    #endregion
+        #region begin task-1
+        Console.WriteLine($"===== Задание 1 =====");
+        Console.Write("Введите сторону квадрата: ");
+        string a = Console.ReadLine();
+        bool result = int.TryParse(a, out int number);
+        if (result)
+        {
+            int p = CalculateThePerimeterOfSquare(number);
+            Console.WriteLine($"Периметр квадрата со стороной a={a} равана {p}");
+        }
+        #endregion
 
-    #region begin task-2
-    Console.WriteLine($"\n===== Задание 2 =====");
-    if (result)
-    {
-        double dNumber = Convert.ToDouble(number);
-        double s = Math.Pow(dNumber, 2);
-        Console.WriteLine($"Площадь квадрата со стороной a={a} равана {s}");
-    }
-    #endregion
-}
+        #region begin task-2
+        Console.WriteLine($"\n===== Задание 2 =====");
+        if (result)
+        {
+            double dNumber = Convert.ToDouble(number);
+            double s = CalculateTheAreaOfSquare(number);
+            Console.WriteLine($"Площадь квадрата со стороной a={a} равана {s}");
+        }
+        #endregion
 
-{
-    #region begin task-3
-    Console.WriteLine($"\n===== Задание 3 =====");
-    Console.Write("Введите 1-ую сторуну прямоугольника: ");
-    string a  = Console.ReadLine();
+        #region begin task-3
+        Console.WriteLine($"\n===== Задание 3 =====");
+        Console.Write("Введите 1-ую сторуну прямоугольника: ");
+        a  = Console.ReadLine();
 
-    Console.Write("Введите 2-ую сторуну прямоугольника: ");
-    string b = Console.ReadLine();
+        Console.Write("Введите 2-ую сторуну прямоугольника: ");
+        string b = Console.ReadLine();
 
-    bool resultA = int.TryParse(a, out int numberA);
-    bool resultB = int.TryParse(b, out int numberB);
+        bool resultA = int.TryParse(a, out int numberA);
+        bool resultB = int.TryParse(b, out int numberB);
     
-    if (resultA && resultB)
-    {
-        int p = 2 * (numberA + numberB);
-        int s = numberA * numberB;
+        if (resultA && resultB)
+        {
+            int p = 2 * (numberA + numberB);
+            int s = numberA * numberB;
 
-        Console.WriteLine($"Периметр прямоугольника P={p}");
-        Console.WriteLine($"Площадь прямоугольника S={s}");
-    }
-    #endregion
-}
+            Console.WriteLine($"Периметр прямоугольника P={p}");
+            Console.WriteLine($"Площадь прямоугольника S={s}");
+        }
+        #endregion
 
-#region if if-t1
-Console.WriteLine("===== Задача 6 =====");
-Console.Write("Введиете 1-ое число: ");
-string firstInput = Console.ReadLine();
+        #region if if-t1
+        Console.WriteLine("===== Задача 6 =====");
+        Console.Write("Введиете 1-ое число: ");
+        string firstInput = Console.ReadLine();
 
-Console.Write("Введиете 2-ое число: ");
-string secondInput = Console.ReadLine();
+        Console.Write("Введиете 2-ое число: ");
+        string secondInput = Console.ReadLine();
 
-Console.Write("Введиете 3-ье число: ");
-string thirdInput = Console.ReadLine();
-int minNum = MinNumber(firstInput, secondInput, thirdInput);
-Console.WriteLine($"Наименьшее число {minNum} среди [{firstInput}, {secondInput}, {thirdInput}]");
-int MinNumber(string first, string second, string third)
-{
-    int firstNum = int.Parse(first);
-    int secondNum = int.Parse(second);
-    int thirdNum = int.Parse(third);
+        Console.Write("Введиете 3-ье число: ");
+        string thirdInput = Console.ReadLine();
+        int minNum = MinNumber(firstInput, secondInput, thirdInput);
+        Console.WriteLine($"Наименьшее число {minNum} среди [{firstInput}, {secondInput}, {thirdInput}]");
+        int MinNumber(string first, string second, string third)
+        {
+            int firstNum = int.Parse(first);
+            int secondNum = int.Parse(second);
+            int thirdNum = int.Parse(third);
 
-    if (firstNum < secondNum && firstNum < thirdNum)
-        return firstNum;
-    else if (secondNum < firstNum && secondNum < thirdNum)
-        return secondNum;
-    else
-        return thirdNum;
+            if (firstNum < secondNum && firstNum < thirdNum)
+                return firstNum;
+            else if (secondNum < firstNum && secondNum < thirdNum)
+                return secondNum;
+            else
+                return thirdNum;
 
-}
-Console.WriteLine();
-#endregion
+        }
+        Console.WriteLine();
+        #endregion
 
-#region integer integer-t1
+        #region integer integer-t1
 
-Console.Write("Введите двузначное число: ");
-string input = Console.ReadLine();
-int sumInput = SumNumber(input);
-int multiplyInput = MultiplyNumber(input);
-int SumNumber(string number)
-{
-    int num = int.Parse(number);
-    int sum = 0;
-    if (num >= 10 && num <= 99)
-    {
-        int firstDigit = num / 10; // Получаем первую цифру
-        int secondDigit = num % 10; // Получаем вторую цифру
-        sum = firstDigit + secondDigit;
+        Console.Write("Введите двузначное число: ");
+        string input = Console.ReadLine();
+        int sumInput = SumNumber(input);
+        int multiplyInput = MultiplyNumber(input);
+        //int SumNumber(string number)
+        //{
+        //    int num = int.Parse(number);
+        //    int sum = 0;
+        //    if (num >= 10 && num <= 99)
+        //    {
+        //        int firstDigit = num / 10; // Получаем первую цифру
+        //        int secondDigit = num % 10; // Получаем вторую цифру
+        //        sum = firstDigit + secondDigit;
 
-        Console.WriteLine($"Сумма цифр числа {number} равна {sum}");
-    }
-    return sum;
-}
+        //        Console.WriteLine($"Сумма цифр числа {number} равна {sum}");
+        //    }
+        //    return sum;
+        //}
 
-int MultiplyNumber(string number)
-{
-    int num = int.Parse(number);
-    int multiply = 0;
-    if (num >= 10 && num <= 99)
-    {
-        int firstDigit = num / 10; // Получаем первую цифру
-        int secondDigit = num % 10; // Получаем вторую цифру
-        multiply = firstDigit * secondDigit;
+        //int MultiplyNumber(string number)
+        //{
+        //    int num = int.Parse(number);
+        //    int multiply = 0;
+        //    if (num >= 10 && num <= 99)
+        //    {
+        //        int firstDigit = num / 10; // Получаем первую цифру
+        //        int secondDigit = num % 10; // Получаем вторую цифру
+        //        multiply = firstDigit * secondDigit;
 
-        Console.WriteLine($"Сумма цифр числа {number} равна {multiply}");
-    }
-    return multiply;
-}
+        //        Console.WriteLine($"Сумма цифр числа {number} равна {multiply}");
+        //    }
+        //    return multiply;
+        //}
 
-#endregion
+        #endregion
+        #region integer integer-t2
+        Console.WriteLine("===== Задача 5 =====");
+        Console.Write("Введите число для A = ");
+        string inputA = Console.ReadLine();
+        Console.Write("Введите число для B = ");
+        string inputB = Console.ReadLine();
+        bool resultCheckCondition = CheckCondition(inputA, inputB);
+        bool CheckCondition(string strA, string strB)
+        {
+            int intA = int.Parse(strA);
+            int intB = int.Parse(strB);
 
-#region integer integer-t2
-Console.WriteLine("===== Задача 5 =====");
-Console.Write("Введите число для A = ");
-string inputA = Console.ReadLine();
-Console.Write("Введите число для B = ");
-string inputB = Console.ReadLine();
-bool resultCheckCondition = CheckCondition(inputA, inputB);
-bool CheckCondition(string strA, string strB)
-{
-    int intA = int.Parse(strA);
-    int intB = int.Parse(strB);
+            if (intA > 2 && intB <= 2)
+            {
+                Console.WriteLine("Условие A > 2 и B <= 3 выполнилось");
+                return true;
+            }
 
-    if (intA > 2 && intB <= 2)
-    {
-        Console.WriteLine("Условие A > 2 и B <= 3 выполнилось");
-        return true;
-    }
+            else
+            {
+                Console.WriteLine("Условие A > 2 и B <= 3 невыполнилось");
+                return false;
+            }
 
-    else
-    {
-        Console.WriteLine("Условие A > 2 и B <= 3 невыполнилось");
-        return false;
-    }
+        }
+        Console.WriteLine();
+        #endregion
+        #region case case-t1 
+        Console.WriteLine("===== Задача 7 =====");
+        Console.Write("Введите цифру от 1-7: ");
+        string inputDayOfWeek = Console.ReadLine();
 
-}
-Console.WriteLine();
-#endregion
+        switch(inputDayOfWeek)
+        {
+            case "1":
+                Console.WriteLine($"Понедельник соответствует цифре {inputDayOfWeek}");
+                break;
+            case "2":
+                Console.WriteLine($"Вторник соответствует цифре {inputDayOfWeek}");
+                break;
+            case "3":
+                Console.WriteLine($"Среда сответствует цифре {inputDayOfWeek}");
+                break;
+            case "4":
+                Console.WriteLine($"Четверг соответствует цифре {inputDayOfWeek}");
+                break;
+            case "5":
+                Console.WriteLine($"Пятница соответствует цифре {inputDayOfWeek}");
+                break;
+            case "6":
+                Console.WriteLine($"Суббота соответствует цифре {inputDayOfWeek}");
+                break;
+            case "7":
+                Console.WriteLine($"Воскресенье соответствует цифре {inputDayOfWeek}");
+                break;
+            default:
+                break;
+        }
+        Console.WriteLine();
+        #endregion
+        #region while while-t1 
+        Console.WriteLine("===== Задача 9 =====");
+        Console.Write("Введите сумму 0 < P <= 25 которая в месяц будет прибавляться к вкладу: ");
+        int percent = int.Parse(Console.ReadLine());
+        FindDepositAndCountMounths(percent);
+        void FindDepositAndCountMounths(int per)
+        {
+            int deposite = 1000;
+            int countMouths = 0;
+            while (deposite < 1100)
+            {
+                deposite += per;
+                countMouths++;
+            }
+            Console.WriteLine($"От 1000 до {deposite} за {countMouths} месяц(а/ев) при сумме процента {per}");
 
-#region case case-t1 
-Console.WriteLine("===== Задача 7 =====");
-Console.Write("Введите цифру от 1-7: ");
-string inputDayOfWeek = Console.ReadLine();
+        }
+        Console.WriteLine();
+        #endregion
 
-switch(inputDayOfWeek)
-{
-    case "1":
-        Console.WriteLine($"Понедельник соответствует цифре {inputDayOfWeek}");
-        break;
-    case "2":
-        Console.WriteLine($"Вторник соответствует цифре {inputDayOfWeek}");
-        break;
-    case "3":
-        Console.WriteLine($"Среда сответствует цифре {inputDayOfWeek}");
-        break;
-    case "4":
-        Console.WriteLine($"Четверг соответствует цифре {inputDayOfWeek}");
-        break;
-    case "5":
-        Console.WriteLine($"Пятница соответствует цифре {inputDayOfWeek}");
-        break;
-    case "6":
-        Console.WriteLine($"Суббота соответствует цифре {inputDayOfWeek}");
-        break;
-    case "7":
-        Console.WriteLine($"Воскресенье соответствует цифре {inputDayOfWeek}");
-        break;
-    default:
-        break;
-}
-Console.WriteLine();
-#endregion
+        #region case case-t1 
+        Console.WriteLine("===== Задача 10 =====");
+        Console.Write("Введите коэффициент A (A != 0): ");
+        int coefficientA = int.Parse(Console.ReadLine());
 
-#region while while-t1 
-Console.WriteLine("===== Задача 9 =====");
-Console.Write("Введите сумму 0 < P <= 25 которая в месяц будет прибавляться к вкладу: ");
-int percent = int.Parse(Console.ReadLine());
-FindDepositAndCountMounths(percent);
-void FindDepositAndCountMounths(int per)
-{
-    int deposite = 1000;
-    int countMouths = 0;
-    while (deposite < 1100)
-    {
-        deposite += per;
-        countMouths++;
-    }
-    Console.WriteLine($"От 1000 до {deposite} за {countMouths} месяц(а/ев) при сумме процента {per}");
+        Console.Write("Введите коэффициент B: ");
+        int coefficientB = int.Parse(Console.ReadLine());
 
-}
-Console.WriteLine();
-#endregion
+        Console.Write("Введите коэффициент C: ");
+        int coefficientC = int.Parse(Console.ReadLine());
+        int countRoots = ReturnTheRootsOfTheEquation(coefficientA, coefficientB, coefficientC);
+        Console.WriteLine($"Количестов корней: {countRoots}   для уравнения {coefficientA}x^2+{coefficientB}x+{coefficientC}=0");
+        int ReturnTheRootsOfTheEquation(int a, int b, int c)
+        {
+            float D = b*b - 4*a*c;
+            if (D < 0)
+                return 0;
+            else if (D > 0)
+                return 2;
+            else
+                return 1;
   
-  #region case case-t1 
-Console.WriteLine("===== Задача 10 =====");
-Console.Write("Введите коэффициент A (A != 0): ");
-int coefficientA = int.Parse(Console.ReadLine());
+        }
+        #endregion  
 
-Console.Write("Введите коэффициент B: ");
-int coefficientB = int.Parse(Console.ReadLine());
+    }
+    //Для 1-го задания
+    public static int CalculateThePerimeterOfSquare(int num)
+    {
+        return num * 4;
+    }
+    //Для 2-го задания
+    public static double CalculateTheAreaOfSquare(double num)
+    {
+        return Math.Pow(num, 2);
+    }
 
-Console.Write("Введите коэффициент C: ");
-int coefficientC = int.Parse(Console.ReadLine());
-int countRoots = ReturnTheRootsOfTheEquation(coefficientA, coefficientB, coefficientC);
-Console.WriteLine($"Количестов корней: {countRoots}   для уравнения {coefficientA}x^2+{coefficientB}x+{coefficientC}=0");
-int ReturnTheRootsOfTheEquation(int a, int b, int c)
-{
-    float D = b*b - 4*a*c;
-    if (D < 0)
-        return 0;
-    else if (D > 0)
-        return 2;
-    else
-        return 1;
-  
+
+    //Для 4-го задания
+    public static int SumNumber(string number)
+    {
+        int num = int.Parse(number);
+        int sum = 0;
+        if (num >= 10 && num <= 99)
+        {
+            int firstDigit = num / 10; // Получаем первую цифру
+            int secondDigit = num % 10; // Получаем вторую цифру
+            sum = firstDigit + secondDigit;
+
+            Console.WriteLine($"Сумма цифр числа {number} равна {sum}");
+        }
+        return sum;
+    }
+
+    //Для 4-го задания
+    public static int MultiplyNumber(string number)
+    {
+        int num = int.Parse(number);
+        int multiply = 0;
+        if (num >= 10 && num <= 99)
+        {
+            int firstDigit = num / 10; // Получаем первую цифру
+            int secondDigit = num % 10; // Получаем вторую цифру
+            multiply = firstDigit * secondDigit;
+
+            Console.WriteLine($"Сумма цифр числа {number} равна {multiply}");
+        }
+        return multiply;
+    }
 }
-#endregion
---
+++ ПовторениеTests/ProgramTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        [DataRow(4, 16)]
+        [DataRow(5, 20)]
+        public void CalculateThePerimeterOfSquareTest(int side, int exceptedValue)
+        {
+            var result = Program.CalculateThePerimeterOfSquare(side);
+            Assert.AreEqual(exceptedValue, result);
+        }
+
+        [TestMethod()]
+        [DataRow(4, 16)]
+        [DataRow(5, 25)]
+        public void CalculateTheAreaOfSquareTest(double side, double exceptedValue)
+        {
+            var result = Program.CalculateTheAreaOfSquare(side);
+            Assert.AreEqual(exceptedValue, result);
+        }
+
+        [TestMethod()]
+        [DataRow("12", 3)]
+        [DataRow("47", 11)]
+        public void SumNumberTest(string number, int exceptedValue)
+        {
+            var result = Program.SumNumber(number);
+            Assert.AreEqual(exceptedValue, result);
+        }
+
+        [TestMethod()]
+        [DataRow("12", 2)]
+        [DataRow("47", 28)]
+        public void MultiplyNumberTest(string number, int exceptedValue)
+        {
+            var result = Program.MultiplyNumber(number);
+            Assert.AreEqual(exceptedValue, result);
+        }
+
+    }
+}
